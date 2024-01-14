f3b8b961c1ecf4ecb06f76471a86d5a83a07adf2
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2024-01-14 14:06:39 -0600

    Create Book model and switch to postgresql

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+books = [
+  {title: 'Book 1', author: "Tom"},
+  {title: 'Book 2', author: "John"},
+  {title: 'Book 3', author: "Mike"},
+]
+
+Book.create!(books)
