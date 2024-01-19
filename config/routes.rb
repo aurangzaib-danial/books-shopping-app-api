81de4ef9f48b48aa4be80c754028cb0a4cc28c72
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2024-01-19 07:55:53 -0600

    Create genre model and setup test data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books, only: [:index, :show]
   post 'sign_in', action: :create, controller: :sign_in
   post 'sign_up', action: :create, controller: :sign_up
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-books = [
-  {title: 'Text processing with Java', author: "Tom", price: 20.95},
-  {title: 'Currently away', author: "John", price: 31.95},
-  {title: 'C Brain Teasers', author: "Mike", price: 45.95}
-]
+book_description = <<-TEXT
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate animi assumenda, quis earum expedita quidem nobis nostrum atque dolorem quo enim quia sed suscipit corrupti saepe nisi fugit? Sed, inventore.
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum sunt voluptates dolores ut veniam sed aliquam delectus et quidem commodi ad praesentium eligendi a pariatur repellat sequi, minima unde expedita.
+TEXT
 
-Book.create!(books)
+50.times do |i|
+  genre = Genre.find_or_create_by!(name: Faker::Book.genre)
+  book = Book.create!(
+    title: Faker::Book.unique.title, 
+    author: Faker::Book.author,
+    genre: genre,
+    description: book_description,
+    price: 9.99 + i
+    )
+end
 
 User.create! email: 'aurangzaib.danial@gmail.com', password: '123456'
